918047924583342af122dcc1a4877f36e15c9411
sakayanagi011 <mikocalderon123@gmail.com>
2024-08-12 01:52:48 -0700

    Commit: navButtonsFinish

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import '../styles/Components/Buttons.css'
 
 interface NavigationButtonProps {
@@ -11,9 +10,9 @@ export const NavigationButton: React.FC<NavigationButtonProps> = ({ icon, name }
 
     return (
         <a
-        href='https://www.vshojo.com/' className={`border-solid border-[1px] border-black rounded-full h-[38px] w-[38px] flex items-center justify-center nav-btn nav-btn--${iconName}`}>
-            <img className='h-[18px] px-[0.59375rem]' src={icon} alt={`${iconName}`} />
-            <span className='text-[18px] hidden pr-[0.59375rem]'>{name}</span>
+        href='https://www.vshojo.com/' className={`border-solid border-[1px] border-black rounded-full min-h-[38px] w-[38px] flex items-center justify-center nav-btn nav-btn--${iconName}`}>
+            <img className='min-h-[18px]' src={icon} alt={`${iconName}`} />
+            <span className='text-[18px] opacity-0'></span>
         </a>
     );
 };
